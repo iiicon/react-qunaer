e0d1a95d784566bd4e31f43fb17233c8a9df81b8
shiguangwei <shigw5420@joyowo.com>
2020-03-29 18:48:55 +0800

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,138 @@
-import React, { useState, PureComponent, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useRef, memo } from "react";
+import { createAdd, createRemove, createSet, createToggle } from "./actions";
+import reducer from "./reducers";
 // import logo from "./logo.svg";
 import "./App.css";
 
-// 自定义hook函数 use开头 抽离组件逻辑
-function useCount(defaultProps) {
-  const [count, setCount] = useState(defaultProps);
-  return [count, setCount];
-}
+let store = {
+  todos: [],
+  incrementCount: 0
+};
 
-// 返回 jsx, 用hooks参数代替props传递参数
-function useCounter(count) {
-  const  size = useSize()
-  return (
-    <h2 style={{ width: "100px", height: "100px", border: "1px solid red" }}>
-      {JSON.stringify(count)} {JSON.stringify(size)}
-    </h2>
-  );
-}
+function bindActionCreators(actionsCreators, dispatch) {
+  const result = {};
 
-function useSize() {
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight
-  });
-
-  const onResize = useCallback(() => { // 如果不加 useCallback 会多次执行
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-    });
-  }, [])
-
-  useEffect(() => {
-    window.addEventListener("resize", onResize, false);
-
-    return () => {
-      window.removeEventListener("resize", onResize, false);
+  for (let key in actionsCreators) {
+    result[key] = function(...args) {
+      dispatch(actionsCreators[key](...args));
     };
-  }, []);
+  }
 
-  return size
+  return result;
 }
 
-// class Counter extends PureComponent {
-//   speak() {
-//     console.log("speak");
-//   }
-//   render() {
-//     const { props } = this;
-//     return (
-//       <h2
-//         style={{ width: "100px", height: "100px", border: "1px solid red" }}
-//       >
-//         {JSON.stringify(props)}
-//       </h2>
-//     );
-//   }
-// }
+const Control = memo(function Control(props) {
+  const { addTodo } = props;
+  const inputRef = useRef();
+  const onSubmit = e => {
+    e.preventDefault();
+    const newText = inputRef.current.value.trim();
+    if (newText.length === 0) return;
+    // dispatch(createAdd({ id: ++isId, text: newText, complete: false }));
+    // 改写
+    addTodo(newText);
+    inputRef.current.value = "";
+  };
+  return (
+    <div className="control">
+      <h1>todos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          ref={inputRef}
+          className="new-todo"
+          placeholder="what needs to be done!"
+        />
+      </form>
+    </div>
+  );
+});
+
+const TodoItem = memo(function TodoItem(props) {
+  const {
+    todo: { id, text, complete },
+    removeTodo,
+    toggleTodo
+  } = props;
+  const onChange = () => {
+    toggleTodo(id);
+  };
+  const onRemove = () => {
+    removeTodo(id);
+  };
+
+  return (
+    <li className="todo-item">
+      <input type="checkbox" onChange={onChange} checked={complete} />
+      <label htmlFor="" className={complete ? "complete" : ""}>
+        {text}
+      </label>
+      <button onClick={onRemove}>&#xd7;</button>
+    </li>
+  );
+});
+
+const Todos = memo(function Todos(props) {
+  const { todos, removeTodo, toggleTodo } = props;
+
+  return (
+    <div>
+      <ul>
+        {todos.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              todo={item}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+});
 
 function App(props) {
-  const [count, setCount] = useCount(0); // 普通hooks
-  const Counter = useCounter(count); // 返回jsx
-  const size = useSize() // 逻辑复用
+  const [todos, setTodos] = useState([]);
+  const [incrementCount, setIncrementCount] = useState(0);
+
+  useEffect(() => {
+    Object.assign(store, { todos, incrementCount });
+  }, [todos, incrementCount]);
+
+  const dispatch = action => {
+    const setter = { todos: setTodos, incrementCount: setIncrementCount };
+
+    if (typeof action === "function") {
+      action(dispatch, () => store);
+      return
+    }
+    const newState = reducer(store, action);
+    for (let key in newState) {
+      setter[key](newState[key]);
+    }
+  };
+
+  useEffect(() => {
+    const payload = JSON.parse(localStorage.getItem("__todos__")) || [];
+    dispatch(createSet(payload));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("__todos__", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +1
-      </button>
-      count:{count}
-      {Counter} {size.width} {size.height}
-      {/*<Counter count={count} />*/}
+    <div className="todo-list">
+      <Control {...bindActionCreators({ addTodo: createAdd }, dispatch)} />
+      <Todos
+        todos={todos}
+        {...bindActionCreators(
+          { removeTodo: createRemove, toggleTodo: createToggle },
+          dispatch
+        )}
+      />
     </div>
   );
 }
---
+++ src/actions.js
@@ -0,0 +1,35 @@
+export function createSet(payload) {
+  return {
+    type: "set",
+    payload
+  };
+}
+let isId = Date.now();
+
+export function createAdd(text) {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      const { todos } = getState();
+      if (!todos.find(todo => todo.text === text)) {
+        dispatch({
+          type: "add",
+          payload: { id: ++isId, text, complete: false }
+        });
+      }
+    }, 3000);
+  };
+}
+
+export function createRemove(payload) {
+  return {
+    type: "remove",
+    payload
+  };
+}
+
+export function createToggle(payload) {
+  return {
+    type: "toggle",
+    payload
+  };
+}
